2b5d1b9f244b887c4c11e9c8fdeb7ea1ac740a3c
alejandro.castro <alejandro.castro@datumsolutions.net>
2021-02-25 13:41:20 +0000

    feat: excercise 03 buttons

--- ex-tempate/tailwind.config.js
+++ ex-tempate/tailwind.config.js
@@ -1,11 +1,19 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        emerald: colors.emerald,
+        cyan: colors.cyan,
+        rose: colors.rose,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
